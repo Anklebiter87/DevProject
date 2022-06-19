37c1352ddeafc3b68147a27d043810d69e71b325
anklebiter <anklebiter87@gmail.com>
2022-06-19 13:28:22 +0000

    updated index

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-    phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
