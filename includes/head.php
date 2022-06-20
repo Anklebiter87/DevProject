2576fe215b86bab5a410c8077cf71dee22478c1c
anklebiter <anklebiter87@gmail.com>
2022-06-20 15:43:24 +0000

    php header add

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+
+<?php
+
+$head = <<<EOD
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+
+  <!-- Nucleo Icons -->
+  <link href="/static/css/icons.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link href="/static/css/base.css" rel="stylesheet" />
+EOD;
+
+echo $head;
