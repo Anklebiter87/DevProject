d86878ecc3275c4e3567612cf347d5ce9b363f1f
anklebiter <anklebiter87@gmail.com>
2022-06-24 02:40:46 +0000

    test file that should be ignored

--- testfile.php
+++ testfile.php
@@ -1,5 +1,3 @@
 <?php
     require_once 'includes/autoloader.php';
-    $swc = new SWC();
-    $scope = array('character_read');
-    $swc->attempt_authorize($scope, false);
+    $swc = new Logger();
