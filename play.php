ed59ae0b93a48c4bc476e0008f1d2385951e182b
anklebiter <anklebiter87@gmail.com>
2022-07-04 23:58:04 +0000

    added play page

---
+++ play.php
@@ -0,0 +1,52 @@
+<?php
+require_once "includes/baseincludes.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <title>Play Pazaak</title>
+    <?php
+    require_once 'includes/head.php';
+    ?>
+</head>
+
+<body>
+    <div class="row">
+        <div class='card'>
+            <div class='card-header'>
+                <div class="col-12 col-sm-12">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item justify-content-center border">
+                            <a href="index.php">
+                                <button type="button" class="btn btn-sm btn-secondary">
+                                    Back To Games
+                                </button></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class='card-body'>
+                <div class="col-12 col-sm-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="d-flex justify-content-center">
+                                <h2 id="form-title">The SWC Pazaak Gambling Platform</h2>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="col-12 col-sm-12">
+                                <p>Thank you for making it this far, but this is just a demo. If you want to play the real game
+                                    you should head over to <a href="http://swcpiston.com/gambling/pazaak-overview/">SWCPISTON</a>
+                                    where the actualy game server is being hosted.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
